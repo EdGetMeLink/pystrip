47857b229628db894cee6ed15359769ad256b1ab
Mike Deltgen <mike@deltgen.net>
2016-10-29 10:04:13 +0200

    removed unneeded bloc of code

--- pystrip/daemon.py
+++ pystrip/daemon.py
@@ -14,24 +14,6 @@ LOG = logging.getLogger(__name__)
 colors = Color()
 
 
-def setup_logging():
-    LOG_FILE = "pystrip.log"
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.DEBUG)
-    formatter = (
-        '%(asctime)s - %(name)s - %(threadName)s - %(levelname)s - %(message)s')
-    file_handler = logging.handlers.RotatingFileHandler(
-        LOG_FILE, maxBytes=1024*1024, backupCount=5)
-    file_handler = logging.FileHandler(LOG_FILE)
-    file_handler.setLevel(logging.DEBUG)
-    file_handler.setFormatter(formatter)
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
-    console_handler.setFormatter(formatter)
-    root_logger.addHandler(file_handler)
-    root_logger.addHandler(console_handler)
-
-
 class Runner(Thread):
 
     def __init__(self, queue, strip_length, striptype=None):
